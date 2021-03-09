ac34365c50e677b5c88a6b3e9cdf4504802fe9a3
shubhanshu02 <shubhanshu.saxena2002@gmail.com>
2021-03-09 18:57:36 +0530

    File Upload Readability

--- ui/context/storage.js
+++ ui/context/storage.js
@@ -1,18 +1,36 @@
 import { loadFirebase } from "./firebase";
-
+// Firebase instance
 const firebase = loadFirebase();
 
-export const uploadHandler = (files) => {
+export const uploadHandler = (
+  files // Expects an array of files from the event.target.files of file input html
+) => {
+  // Initialize firebase storage
   const storage = firebase.storage();
-  var fullPaths = [];
-  for (let i = 0; i < files.length; i++) {
-    console.log("sss ", files[i].name);
-    const reference = storage.ref("testimages/" + files[i].name);
+  // List to store file paths inside Firebase Storage
+  var filePaths = [];
 
-    reference.put(files[i]).then(() => {
-      console.log("Done");
-      fullPaths.push(reference.fullPath);
-    });
-  }
-  return fullPaths;
+  // Single Promise for all files
+  Promise.all(
+    // Iterate through all the Files and upload one by one under single promise
+    files.map(
+      (
+        file // File object
+      ) => {
+        // Create a reference for the image file
+        // TODO: Change folder name as per the requirement
+        const reference = storage.ref(
+          "testimages/" + // folder name
+            file.name // file name
+        );
+
+        reference
+          .put(file) // Upload the file
+          .then(() => {
+            filePaths.push(reference.fullPath); // Store the file path in an array
+          });
+      }
+    )
+  ).then(() => console.log("DONE"));
+  return filePaths;
 };
--- ui/pages/firebaseTest.jsx
+++ ui/pages/firebaseTest.jsx
@@ -2,16 +2,12 @@ import { useState } from "react";
 import { uploadHandler } from "../context/storage";
 
 export default function firebasPage() {
-  
-
   const handle = (event) => {
-
-    uploadHandler(event.target.files)
-    
+    uploadHandler(event.target.files);
   };
   return (
     <>
-      <input type="file" onChange={handle} multiple />
+      <input type="file" onChange={handle} accept="image/*" multiple />
     </>
   );
 }
