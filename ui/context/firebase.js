4b0501c5cb087364c85983662202b4f7b430e9f4
shubhanshu02 <shubhanshu.saxena2002@gmail.com>
2021-03-09 14:47:32 +0530

    Added Firebase

---
+++ ui/context/firebase.js
@@ -0,0 +1,26 @@
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/storage";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCaoxPJoGQtfw7FPUo8jsyigxJRCqW37Kk",
+  authDomain: "interiit-ui.firebaseapp.com",
+  projectId: "interiit-ui",
+  storageBucket: "interiit-ui.appspot.com",
+  messagingSenderId: "89989048018",
+  appId: "1:89989048018:web:6c45fde555f4323cd7af0b",
+  measurementId: "G-YB3G1L3DJF",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export const loadFirebase = () => {
+  if (typeof window !== "undefined") {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+    }
+    return firebase;
+  }
+};
