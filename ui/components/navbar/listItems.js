8251e5bd5977344003de3f70ed521bc7fa4b7aef
Yash Agarwal <yagarwal1307@gmail.com>
2021-03-10 13:27:38 +0530

    Fix all the links

--- ui/components/navbar/listItems.js
+++ ui/components/navbar/listItems.js
@@ -8,18 +8,22 @@ import Link from 'next/link'
 
 export const mainListItems = (
   <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Statistics" />
-    </ListItem>
+    <Link href='/' passHref>
+      <ListItem button component='a'>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+    </Link>
+    <Link href='/dashboard' passHref>
+      <ListItem button component='a'>
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Statistics" />
+      </ListItem>
+    </Link>
   </div>
 );
 
