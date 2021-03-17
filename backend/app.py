62b770630353fce8d99099b1fd1cfed220b4fe0d
Mainak Samanta <mainak.samanta.ece19@itbhu.ac.in>
2021-03-17 22:51:12 +0530

    model added

--- backend/app.py
+++ backend/app.py
@@ -4,10 +4,12 @@ import pickle
 import numpy as np
 from db import load_latest_model_from_db
 from PIL import Image
+import sys
 import torch
 from torch.autograd import Variable
 from torchvision import transforms
-
+sys.path.insert(1, '/gtsrb_base_model/engine/')
+from model import TrafficSignNet
 
 
 app = Flask(__name__)
@@ -19,6 +21,7 @@ parser.add_argument("event")
 parser.add_argument("images")
 parser.add_argument("labels")
 
+DEVICE = torch.device("cpu")
 
 
 
@@ -26,8 +29,9 @@ class PredictImage(Resource):
     def get(self):
         args = parser.parse_args()
         image = Image.open(args['images'])
-        model = load_latest_model_from_db()
-        pred_label, pred_label_proba = predict_image(image, model)
+        model = TrafficSignNet()
+        model = self.load_model('/43_classes.pt', model)# To be edited with load_model_from_pkl
+        pred_label, pred_label_proba = self.predict_image(image, model)
         output_pred = {'pred': pred_label, 'confidence': pred_label_proba}
         return output_pred
 
@@ -37,14 +41,19 @@ class PredictImage(Resource):
         trans_image = Variable(trans_image, requires_grad = True)
         return trans_image
     
-    def predict_image(image, model):
+    def predict_image(self, image, model):
         model.eval()
-        torch_image = load_image(image)
+        torch_image = self.load_image(image)
         with torch.no_grad():
             output = model(torch_image)
             _, pred = torch.max(output, 1)
             pred_proba = output[pred]
         return pred, pred_proba
+    
+    def load_model(self, checkpoint_path, model):
+        checkpoint = torch.load(checkpoint_path, DEVICE)
+        model.load_state_dict(checkpoint['state_dict'])
+        return model
 
 class TrainImages(Resource):
     def get(self):
