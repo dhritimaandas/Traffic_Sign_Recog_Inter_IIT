50946b9924a37232b85cfc8ccb7d45c06d5bdf05
Mainak Samanta <mainak.samanta.ece19@itbhu.ac.in>
2021-03-17 20:14:46 +0530

    prediction added

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,59 @@
 from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+import pickle
+import numpy as np
+from db import load_latest_model_from_db
+from PIL import Image
+import torch
+from torch.autograd import Variable
+from torchvision import transforms
+
+
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello():
+# argument parsing
+parser = reqparse.RequestParser()
+parser.add_argument("event")
+parser.add_argument("images")
+parser.add_argument("labels")
+
+
+class PredictImage(Resource):
+    def get(self):
+        args = parser.parse_args()
+        image = Image.open(args['images'])
+        model = load_latest_model_from_db()
+        pred_label, pred_label_proba = predict_image(image, model)
+        if pred_label == label:
+            acc = 1
+        else:
+            acc = 0
+        output_pred = {'pred': pred_label, 'confidence': pred_label_proba, 'accuracy': acc}
+        return acc
+
+    def load_image(self, image, size=(32, 32)):
+        trans = transforms.Compose([transforms.Resize(size), transforms.ToTensor()])
+        trans_image = trans(image).float()
+        trans_image = Variable(trans_image, requires_grad = True)
+        return trans_image
     
-    return "Hello World!"
+    def predict_image(image, model):
+        model.eval()
+        torch_image = load_image(image)
+        with torch.no_grad():
+            output = model(torch_image)
+            _, pred = torch.max(output, 1)
+            pred_proba = output[pred]
+        return pred, pred_proba
+
+class TrainImages(Resource):
+    def get(self):
+        pass
+
+api.add_resource(PredictImage, '/predict')
+api.add_resource(TrainImages, '/train')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
---
+++ backend/db.py
@@ -0,0 +1,36 @@
+import Pyrebase
+import pickle
+
+#Firebase configuration dictionary
+firebaseConfig = {
+  apiKey: "AIzaSyCaoxPJoGQtfw7FPUo8jsyigxJRCqW37Kk",
+  authDomain: "interiit-ui.firebaseapp.com",
+  projectId: "interiit-ui",
+  storageBucket: "interiit-ui.appspot.com",
+  messagingSenderId: "89989048018",
+  appId: "1:89989048018:web:6c45fde555f4323cd7af0b",
+  measurementId: "G-YB3G1L3DJF",
+}
+
+#Initialize firebase connection
+firebase = pyrebase.initialize_app(firebaseConfig)
+#Initialize firebase database
+db = firebase.database()
+
+def save_model_to_db(model, modelMetrics):
+  #Pickle the model
+  pickled_model = pickle.dumps(model)
+
+  #new model json
+  model_info = {'model': pickled_model, 'model_metrics':modelMetrics, 'created_time': time.time()}
+
+  #Push the json to the database
+  db.child("models").push(model_info)
+
+  return model_info
+
+def load_latest_model_from_db():
+  latest_model = db.child("models").limit_to_last(1).get()  
+  pickled_model = latest_model['model']
+
+  return pickle.loads(pickled_model)
