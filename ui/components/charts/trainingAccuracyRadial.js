dd79f78b309a2467b84dc0995d7e5f93cdcd6472
shubhanshu02 <shubhanshu.e01@gmail.com>
2021-03-24 02:46:51 +0530

    Window Not Defined Corrected

--- ui/components/charts/trainingAccuracyRadial.js
+++ ui/components/charts/trainingAccuracyRadial.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {acc_p} from '../data.json';
-import ReactApexChart from 'react-apexcharts';
+import dynamic from "next/dynamic";
+const ReactApexChart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 export default class TrainingAccuracyRadial extends React.Component{
 
