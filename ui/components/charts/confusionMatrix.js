d3cd3b7484a0dfedce72862b915840c878a0645d
shubhanshu02 <shubhanshu.e01@gmail.com>
2021-03-25 19:36:18 +0530

    Confusion Matrix Waste Code Removal

--- ui/components/charts/confusionMatrix.js
+++ ui/components/charts/confusionMatrix.js
@@ -4,30 +4,12 @@ const ReactApexChart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
 });
 
-var y_pred = [0, 1, 42, 5, 32, 40, 11, 19, 22, 37];
-var y_true = [0, 0, 42, 1, 32, 35, 11, 19, 21, 37];
-
-var matrix = new Array(48).fill(1).map(() => new Array(48).fill(1));
-
-for (var i = 0; i < y_pred.length; i++) matrix[y_pred[i]][y_true[i]] += 1;
-
-var dataSet = new Array();
-
-for (let i = 0; i < matrix.length; i++) {
-  dataSet.push({
-    name: "" + (i + 1),
-    data: matrix[i],
-  });
-}
-
 export default class HeatMap extends React.Component {
   constructor(props) {
     super(props);
 
     var dataSet = new Array();
 
-
-    
     for (let i = 0; i < this.props.matrix.length; i++) {
       dataSet.push({
         name: "" + (i + 1),
